bdd06726df2c1e931f1126fdc54ebbeab373dcef
Siddharth Vinod Kamath <sid@Siddharths-Laptop.local>
2024-11-03 15:13:11 -0800

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from engine import Value
 from nn import MLP
 
-# Sample data (input features and target outputs)
+# Sample data 
 xs = [[2.0, 3.0, -1.0], 
       [3.0, -1.0, 0.5], 
       [0.5, 1.0, 1.0], 
@@ -12,7 +12,7 @@ ys = [0.0, 1.0, 1.0, 0.0]  # Target outputs
 # Initialize the MLP with input and output sizes
 n = MLP(3, [4, 4, 1])
 
-# Training loop
+# Training looop strt
 for k in range(20):
     # Forward pass
     ypred = [n(x) for x in xs]
